7345ed346c455e66dc9c30cb542180a225ee5f81
cole <coleswany@gmail.com>
2019-03-19 00:49:51 -0400

    Fixed bugs in the first 2 features; added in the ‘squad strike’ feature

--- swanyG300spaceshooter/Assets/Scripts/GameController.cs
+++ swanyG300spaceshooter/Assets/Scripts/GameController.cs
@@ -20,10 +20,14 @@ public class GameController : MonoBehaviour
     public GameObject[] hazards;
     public GameObject boss;
     public Vector3 spawnValues;
+    public Vector3 squadStrikepos = new Vector3(9, 0, -15);
+    public Quaternion squadStrikerot = Quaternion.identity;
     public int hazardCount = 10;
     public float spawnWait = 0.75f;
     public float startWait;
     public float waveWait;
+
+    public GameObject squadStrike;
 
     public GUIText scoreText;
     public GUIText restartText;
@@ -33,6 +37,7 @@ public class GameController : MonoBehaviour
     private bool restart;
     private int score;
 
+
     private float timeLimit = 9.0f;
 
     private float timePlayed;
@@ -41,11 +46,13 @@ public class GameController : MonoBehaviour
     private bool boss2 = false;
     private bool boss3 = false;
 
+    private bool squadUsed = false;
+
     void Start()
     {
 
         startWait = 10;
-        musicEv = FMODUnity.RuntimeManager.CreateInstance(musicFMOD);
+        musicEv = FMODUnity.RuntimeManager.CreateInstance(musicFMOD); //<<====creating instances for FMOD events and parameters
         musicEv.getParameter("pointintensity", out pointsParam);
         musicEv.start();
 
@@ -56,7 +63,7 @@ public class GameController : MonoBehaviour
         gameOver = false;
         restart = false;
         restartText.text = "";
-        gameOverText.text = "G300\nSPACE SHOOTER\n\nMove: W-A-S-D or arrows\nShoot: mouse click";
+        gameOverText.text = "G300\nSPACE SHOOTER\n\nMove: W-A-S-D or arrows\nShoot: mouse click\n1-use Squad Strike: space";
         score = 0;
         UpdateScore();
         StartCoroutine(SpawnWaves());
@@ -91,6 +98,19 @@ public class GameController : MonoBehaviour
             vertimove = 0;
         }
 
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (squadUsed == false)
+            {
+                Instantiate(squadStrike, squadStrikepos, squadStrikerot * Quaternion.Euler(0f, -90f, 0f));
+                squadUsed = true;
+            }
+            else
+            {
+                return;
+            }
+        }
+
         if (horizmove == 0 && vertimove == 0)
         {
 
@@ -117,6 +137,7 @@ public class GameController : MonoBehaviour
             }
         }
     }
+
     IEnumerator SpawnWaves()
     {
         yield return new WaitForSeconds(startWait);
@@ -124,6 +145,7 @@ public class GameController : MonoBehaviour
         {
             for (int i = 0; i < hazardCount; i++)
             {
+                gameOverText.text = "";
                 GameObject hazard = hazards[Random.Range(0, hazards.Length)];
                 Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
                 Quaternion spawnRotation = Quaternion.identity;
@@ -147,7 +169,7 @@ public class GameController : MonoBehaviour
         UpdateScore();
     }
 
-    void UpdateScore()
+    void UpdateScore() //changes FMOD params + spawns bosses based on score
     {
         scoreText.text = "Score: " + score;
 
@@ -155,13 +177,13 @@ public class GameController : MonoBehaviour
         {
             pointsParam.setValue(1);
             hazardCount = 20;
-            spawnWait = 0.6f;
+            spawnWait = 0.8f;
         }
         if (score >= 700)
         {
             pointsParam.setValue(2);
             hazardCount = 30;
-            spawnWait = .5f;
+            spawnWait = .6f;
             if (boss1 == true)
             { 
                 return; 
@@ -176,12 +198,12 @@ public class GameController : MonoBehaviour
         {
             pointsParam.setValue(3);
             hazardCount = 100;
-            spawnWait = 0.3f;
+            spawnWait = 0.4f;
             if (boss2 == true)
             {
                 return;
             }
-            else
+            if (boss2 == false)
             {
                 CreateBoss();
                 boss1 = true;
@@ -195,7 +217,7 @@ public class GameController : MonoBehaviour
             {
                 return;
             }
-            else
+            if (boss3 == false)
             {
                 CreateBoss();
                 boss1 = true;
@@ -209,10 +231,12 @@ public class GameController : MonoBehaviour
         gameOver = true;
         vroomvroomEv.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
     }
-    void CreateBoss()
+    void CreateBoss() //creates a boss
     {
         Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
         Quaternion spawnRotation = Quaternion.identity;
         Instantiate(boss, spawnPosition, spawnRotation);
+        Debug.Log("Spawned Boss");
+        return;
     }
 }
---
+++ swanyG300spaceshooter/Assets/SquadStrikeController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SquadStrikeController : MonoBehaviour
+{
+
+    [FMODUnity.EventRef]
+    public string pewpew = "event:/Shoot";
+    FMOD.Studio.EventInstance pewpewEv;
+
+
+    public GameObject shot;
+    public Transform shotSpawn;
+
+
+    private void Start()
+    {
+        StartCoroutine(ShootShoot());
+    }
+
+    private IEnumerator ShootShoot()
+    {
+        Debug.Log("hereiam");
+        WaitForSeconds wait = new WaitForSeconds(0.1f);
+        while(true)
+        {
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+            pewpewEv = FMODUnity.RuntimeManager.CreateInstance(pewpew);
+            pewpewEv.start();
+            yield return wait;
+        }
+    }
+}
