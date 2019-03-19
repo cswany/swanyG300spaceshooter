7cd3c5577127bd59bd3294c5d3b2aa00431e87a8
cole <coleswany@gmail.com>
2019-03-18 20:28:38 -0400

    Massive import; installed FMOD package, put FMOD file in the directory

---
+++ swanyG300spaceshooter/Assets/Prefabs/Boss_DestroyByContact.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boss_DestroyByContact : MonoBehaviour
+{
+    public GameObject explosion;
+    public GameObject playerExplosion;
+    public GameObject bossExplosion;
+    public int scoreValue;
+    private Done_GameController gameController;
+    public float health = 5;
+
+    [FMODUnity.EventRef]
+    public string explosionFMOD = "event:/Explosion";
+    FMOD.Studio.EventInstance explosionEv;
+
+    void Start()
+    {
+        explosionEv = FMODUnity.RuntimeManager.CreateInstance(explosionFMOD);
+
+        GameObject gameControllerObject = GameObject.FindGameObjectWithTag("GameController");
+        if (gameControllerObject != null)
+        {
+            gameController = gameControllerObject.GetComponent<Done_GameController>();
+        }
+        if (gameController == null)
+        {
+            Debug.Log("Cannot find 'GameController' script");
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Boundary" || other.tag == "Enemy")
+        {
+            return;
+        }
+
+        if (health > 1)
+        {
+            health = health - 1;
+            if (other.tag == "Player")
+            {
+               Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+                gameController.GameOver();
+                explosionEv.start();
+            }
+            else
+            {
+                Instantiate(bossExplosion, other.transform.position, transform.rotation);
+                Destroy(other.gameObject);
+            }
+        }
+        else
+        {
+            if (explosion != null)
+            {
+                Instantiate(bossExplosion, transform.position, transform.rotation);
+                explosionEv.start();
+            }
+
+            if (other.tag == "Player")
+            {
+                Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+                gameController.GameOver();
+                explosionEv.start();
+            }
+
+            gameController.AddScore(scoreValue);
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ swanyG300spaceshooter/Assets/Prefabs/Boss_WeaponController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boss_WeaponController : MonoBehaviour
+{
+    public GameObject shot;
+    public Transform shotSpawn1;
+    public Transform shotSpawn2;
+    public float fireRate;
+    public float delay;
+
+    [FMODUnity.EventRef]
+    public string enemyFMOD = "event:/EnemyShoot";
+    FMOD.Studio.EventInstance enemyEv;
+
+
+    void Start()
+    {
+        InvokeRepeating("Fire", delay, fireRate);
+        enemyEv = FMODUnity.RuntimeManager.CreateInstance(enemyFMOD);
+    }
+
+    void Fire()
+    {
+        Instantiate(shot, shotSpawn1.position, shotSpawn1.rotation);
+        Instantiate(shot, shotSpawn2.position, shotSpawn2.rotation);
+        /* V334 */
+        enemyEv.start();
+        // original shot fired audio call was here //
+        Debug.Log("ENEMY fires");
+    }
+}
